c0835a063086c73395e3c7f69564ba03ee5dce5c
DiegoMolina04 <37599110+DiegoMolina04@users.noreply.github.com>
2018-03-20 16:56:46 -0500

    Update OperadorFracciones.java

--- ProblemasURI/OperadorFracciones.java
+++ ProblemasURI/OperadorFracciones.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class OperadorFracciones {
-
+	//realiza operaciones con fraccionarios 
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
 		BufferedWriter bw = new BufferedWriter( new OutputStreamWriter(System.out) );
