d396f83aebd24808d453994e3c9e1ce5bb9e9797
DiegoMolina04 <37599110+DiegoMolina04@users.noreply.github.com>
2018-03-20 17:03:21 -0500

    Update LongATMQueue.java

--- Parciales/LongATMQueue.java
+++ Parciales/LongATMQueue.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class LongATMQueue {
-
+	//Mira cuantos grupos hay en una fila frente a un ATM
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
